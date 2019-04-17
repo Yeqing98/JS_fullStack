37c00d66e18f4903b0ba17dc21d026eeb41375c8
Yeqing98 <1835132202@qq.com>
2019-04-17 21:41:06 +0800

    学习拖拽，学习算法，学习webgl

---
+++ node/walk_directory/index.js
@@ -0,0 +1,34 @@
+const fs = require('fs');
+const files = [];
+function walk(path){
+    fs
+        .readdirSync(path)
+        .forEach(file => {
+            const newPath = path + '/' + file;
+            const stat = fs.statSync(newPath);
+            // console.log(file,'+++++');
+            if(stat.isFile){
+                //是js文件嘛
+                // console.loh('isFile');
+                if(/\.js/.test(file)){
+                    files.push(file);
+                }
+                // console.log(file);
+        
+            }else if(stat.isDirectory()){
+                console.log('isDirectory');
+                walk(newPath);
+            }
+        })
+
+    // fs
+    //     .readdir(path,function(error, files){
+    //         if(error){
+    //             console.log(error);
+    //             return;
+    //         }
+    //         console.log(files);
+    //     })
+}
+walk('./src');
+console.log(files,'----');
