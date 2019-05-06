d3e27507f6a073c08af5fdee3e861b15b729af57
Yeqing98 <1835132202@qq.com>
2019-05-06 21:35:35 +0800

    学习vant weapp

---
+++ wxapp/vant-cloud/miniprogram/pages/index/index.js
@@ -0,0 +1,97 @@
+// miniprogram/pages/index/index.js
+import Notify from '../dist/notify/notify';
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    show: false,
+    groupName: ''
+  },
+  onGroupNameChange(event) {
+    // console.log(event)
+    this.setData({
+      groupName: event.detail
+    })
+  },
+  showNewGroupModal() {
+    this.setData({
+      show: true
+    })
+  },
+  closeDialog() {
+    this.setData({
+      show: false
+    })
+  },
+  createGroup() {
+    let that = this
+    if(that.data.groupName === '') {
+      Notify({
+        text: '起个名字吧',
+        duration: 1500,
+        selector: '#notify-selector',
+        backgroundColor: '#dc3545'
+      });
+      that.selectComponent('#new-group-model').stopLoading()
+      return
+    } else {
+      
+    }
+  },
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
