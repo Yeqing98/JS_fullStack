f09c4c11ca674fb16c229262214c2e30678ad65a
Yeqing98 <1835132202@qq.com>
2019-05-31 17:02:38 +0800

    学习router内部内容

---
+++ VUE/vue-router/router/src/components/LookOne.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="look-one">{{msg}}
+        <div class="go-two" @click='goTwo'>去look2</div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'LookOne',
+    data () {
+        return {
+            msg: '你瞅啥'
+        }
+    },
+    methods: {
+        goTwo () {
+            // this.$router.push('/look/look-two')
+            // this.$router.push({ path: '/look/look-two'})
+            this.$router.push({ name: 'lookTwo', params: {id: 12345}})
+        }
+    },
+    beforeRouteEnter (to, from, next) {
+        console.log(from, to)
+        next()
+    },
+    beforeRouteLeave (to, from, next) {
+        console.log(from, to)
+        next()
+    }
+}
+</script>
---
+++ VUE/vue-router/router/src/components/LookTwo.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="look-two">{{msg}}
+        {{$route.params.id}}
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'LookTwo',
+    data () {
+        return {
+            msg: '瞅你咋地'
+        }
+    }
+}
+</script>
