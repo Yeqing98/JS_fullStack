0e0db7853ef2b61bea9ea4d025c4ce4c868f068b
Yeqing98 <1835132202@qq.com>
2019-06-04 21:36:26 +0800

    学习koa

---
+++ node/koa-template-B/router.js
@@ -0,0 +1,54 @@
+/**
+ * 管理 router
+ */
+const Router = require('koa-router');
+const router = new Router();
+const user = {
+    name: '丹丹',
+    posts: [
+        {
+            id: 0,
+            title: '小程序踩坑指南'
+        },
+        {
+            id: 1,
+            title: 'vue.js 浅析 组件数据传递'
+        }
+    ]
+}
+const postsDetail = [
+    {
+        id: 0,
+        content: 'wechat app'
+    },
+    {
+        id: 1,
+        content: '<strong>react 表示不服</strong>'
+    }
+]
+router.get('/user', async (ctx) => {
+    await ctx.render('user', { user });
+})
+router.get('/posts', async (ctx) => {
+    const { id } = ctx.query;
+    const post = postsDetail.find(postItem => postItem.id == id);
+    await ctx.render('post', { post });
+})
+router.get('/create',async (ctx) => {
+    await ctx.render('create');
+})
+router.post('/create',async (ctx) => {
+    console.log(ctx.request.body);
+    const {title, content} = ctx.request.body;
+    let id = Date.now();
+    user.posts.push({
+        id,
+        title
+    })
+    postsDetail.push({
+        id,
+        content
+    })
+    ctx.redirect('/user');
+})
+module.exports = router;
