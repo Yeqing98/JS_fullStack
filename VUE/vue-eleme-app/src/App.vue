4b49c2a92788d0e7827f5f2622ea1cd2d7b4b91f
Yeqing98 <1835132202@qq.com>
2019-06-12 21:47:13 +0800

    学习vue

---
+++ VUE/vue-eleme-app/src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <div id="app">
+    <v-header :seller='seller'></v-header>
+  </div>
+</template>
+
+<script>
+import header from '@/components/header/header.vue'
+export default {
+  name: 'App',
+  data () {
+    return {
+      seller: {
+        // 从api获取
+      }
+    }
+  },
+  components: {
+    'v-header': header
+  },
+  created () {
+    this.$http.get('https://www.easy-mock.com/mock/5ca2c29464930718b239eb94/lm/vue-eleme-seller')
+      .then(res => {
+        console.log(res)
+        if (res.data.errno === 0) {
+          this.seller = Object.assign({}, this.seller, res.data.data)
+        }
+      })
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ mp-vue/todolist/note.md
@@ -0,0 +1,10 @@
+## 
+miniprogramRoot: ''
+小程序的源码目录
+整个目录  被mpvue  定制过了
+
+## dist/wx
+npm run dev 打包
+
+## 作用
+vue 的语法写小程序
